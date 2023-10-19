e1ab54b98da7fcbd3b6d2ed94f226478d8bfe065
Niall Kelly <niall.kelly@researchgate.net>
2023-10-19 15:52:00 +0200

    Fix test

--- dags/example_dag.py
+++ dags/example_dag.py
@@ -16,6 +16,7 @@ import datetime
 from airflow import models
 from airflow.operators import bash
 from airflow.models.variable import Variable
+from google.cloud import secretmanager
 
 # If you are running Airflow in more than one time zone
 # see https://airflow.apache.org/docs/apache-airflow/stable/timezone.html
@@ -33,6 +34,12 @@ default_args = {
     "start_date": YESTERDAY,
 }
 
+def get_secret_value(secret_name, project_id):
+    client = secretmanager.SecretManagerServiceClient()
+    secret_version = client.access_secret_version(name=f"projects/{project_id}/secrets/{secret_name}/versions/latest")
+    return secret_version.payload.data.decode("UTF-8")
+
+
 with models.DAG(
         "composer_sample_dag",
         "catchup=False",
@@ -40,7 +47,14 @@ with models.DAG(
         schedule_interval=datetime.timedelta(days=1),
 ) as dag:
     # Print the dag_run id from the Airflow logs
-    secret = Variable.get('example-secret')
+    #secret = Variable.get('example-secret')
+
+    secret_name = "example-secret"
+    project_id = "qwiklabs-gcp-03-d8830502f2ec"
+
+    secret_value = get_secret_value(secret_name, project_id)
+
+
     print_dag_run_conf = bash.BashOperator(
-        task_id="print_dag_run_conf", bash_command="secret:" + secret
+        task_id="print_dag_run_conf", bash_command="secret:" + secret_value
     )
